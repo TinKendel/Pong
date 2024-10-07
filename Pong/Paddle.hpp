de44d368408e2f8fe3a7a7209721c0888b0e23c1
TinKendel <kendel.tin@gmail.com>
2024-10-07 19:29:52 +0200

    Added function comments.

--- Pong/Paddle.hpp
+++ Pong/Paddle.hpp
@@ -12,12 +12,29 @@ private:
 	sf::Vector2f paddle_velocity;
 
 public:
+	/// \brief Constructor.
+	/// Sets paddle size, color and position.
+	/// \param position Type float. Position tells which paddle is created.
 	Paddle(float& position);
 
+	/// \brief Gets the paddle address.
+	/// \returns Address of paddle.
 	sf::RectangleShape& GetPaddle();
 
+	/// \brief Handles the input for paddle movement.
+	///
+	/// \param speed Type float. Defines the speed at witch the paddle is moving.
+	/// \param Up Type sf::Keyboard::Key. The key that moves the paddle upwards.
+	/// \param Down Type sf::Keyboard::Key. The key that moves the paddle downwards.
 	void InputHandling(float& speed, sf::Keyboard::Key const& Up, sf::Keyboard::Key const& Down) override;
+
+	/// \brief Moves the paddle.
 	void PositionUpdate() override;
+
+	/// \brief Draws the paddle on the window.
+	/// \param window Type sf::RenderWindow. Window is used for drawing 2D shapes.
 	void Drawing(sf::RenderWindow& window) override;
+
+	/// \brief Detects if the paddle is touching the top or bottom of the window.
 	void CollisionDetection() override;
 };
--- Pong/Wall.hpp
+++ Pong/Wall.hpp
@@ -8,12 +8,24 @@ private:
 	sf::RectangleShape line;
 
 public:
-
+	/// \brief Constructor.
+	/// Sets up the size and color of the wall.
 	Wall();
+
+	/// \brief Returns wall address.
+	/// \returns Wall address.
 	sf::RectangleShape& GetLine();
 
+	/// \brief This function is not used.
 	void InputHandling(float& speed, sf::Keyboard::Key const& Up, sf::Keyboard::Key const& Down) override;
+
+	/// \brief This function is not used.
 	void PositionUpdate() override;
+
+	/// \brief Draws the wall on the window.
+	/// \param window Type sf::RenderWindow. Window is used for drawing 2D shapes.
 	void Drawing(sf::RenderWindow& window) override;
+
+	/// \brief This function is not used.
 	void CollisionDetection() override;
 };
